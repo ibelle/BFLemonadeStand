ccf94d7f59d870eb86be5f5225a9a788e41a1d53
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-02-21 16:05:37 -0500

    Added initial outlets
    Added initial outlets

---
+++ LemonadeStand/Lemons.swift
@@ -0,0 +1,10 @@
+//
+//  Lemons.swift
+//  LemonadeStand
+//
+//  Created by HUGE | Isaiah Belle on 2/20/15.
+//  Copyright (c) 2015 IsaiahBelleDigital. All rights reserved.
+//
+
+import Foundation
+struct
--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -9,7 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var numLemons:Int = 1
+    var numIceCubes:Int = 1
+    var cashOnHand:Double = 10.00
+    //Containers
+    @IBOutlet weak var suppliesContainerView: UIView!
+    @IBOutlet weak var inventoryContinerView: UIView!
+    @IBOutlet weak var mixContainerView: UIView!
+    @IBOutlet weak var salesContainerView: UIView!
+    
+    //Inventory Outlets
+    @IBOutlet weak var availableCashLabel: UILabel!
+    @IBOutlet weak var lemonInventoryLabel: UILabel!
+    @IBOutlet weak var iceInventoryLabel: UILabel!
+    
+    
+    //Supply Outlets
+    @IBOutlet weak var addRemoveLemons: UIStepper!
+    @IBOutlet weak var addRemoveIce: UIStepper!
+    
+    //Mix Outlets
+    
+    
+    
+    //Sales Outlets
+    
+    
+    //Top Level Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +45,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Supply Actions
+    @IBAction func addRemoveLemonsValueChanged(sender: AnyObject) {
+    }
+
+    @IBAction func addRemoveIceValueChanged(sender: UIStepper) {
+    }
+    
 
 
 }
