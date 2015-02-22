40d348301fdb6819c719e68d757680eea6e59dca
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-02-22 02:00:03 -0500

    added assets and object classes
    added assets and object classes

---
+++ LemonadeStand/Customer.swift
@@ -0,0 +1,12 @@
+//
+//  Customer.swift
+//  LemonadeStand
+//
+//  Created by HUGE | Isaiah Belle on 2/22/15.
+//  Copyright (c) 2015 IsaiahBelleDigital. All rights reserved.
+//
+
+import Foundation
+class Customer {
+    
+}
---
+++ LemonadeStand/Lemonade.swift
@@ -0,0 +1,12 @@
+//
+//  Lemonade.swift
+//  LemonadeStand
+//
+//  Created by HUGE | Isaiah Belle on 2/22/15.
+//  Copyright (c) 2015 IsaiahBelleDigital. All rights reserved.
+//
+
+import Foundation
+struct Lemonade {
+    
+}
