1e48339585cedcf40871083491c7770e5a4e9ca1
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-02-22 22:06:34 -0500

    Added Purchase supply code
    Added Purchase supply code

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Constants
+    let LEMON_PRICE:Int = 2
+    let ICE_PRICE:Int = 1
+    
+    //Supply Vars
     var lemonSupply:Int = 1
     var iceSupply:Int = 1
     var cashSupply:Int = 10
+    
+
     
     //Containers
     @IBOutlet weak var suppliesContainerView: UIView!
@@ -63,19 +70,79 @@ class ViewController: UIViewController {
     
     //Supply Actions
     @IBAction func lemonSupplyValueChanged(sender: UIStepper) {
-        self.additionalLemonSupply = Int(sender.value)
-        //TODO:Update INVENTORY
+        var inputLemonValue = Int(sender.value)
+        
+        if inputLemonValue > self.additionalLemonSupply {
+            purchaseLemon()
+        }else if inputLemonValue < self.additionalLemonSupply{
+            unPurchaseLemon()
+        }
+        
+        self.additionalLemonSupply = inputLemonValue
         updateMainView()
     }
 
     @IBAction func iceSupplyValueChanged(sender: UIStepper) {
-        self.additionalIceSupply = Int(sender.value)
-        //TODO:Update INVENTORY
-        updateMainView()
+        
+        var inputIceCubeValue = Int(sender.value)
+        
+        if inputIceCubeValue > self.additionalIceSupply {
+            purchaseIceCube()
+        }else if inputIceCubeValue < self.additionalIceSupply{
+            unPurchaseIceCube()
+        }
 
+        self.additionalIceSupply = Int(sender.value)
+        updateMainView()
+    }
+    
+    func purchaseLemon() {
+            if self.LEMON_PRICE  <= cashSupply {
+                //Update Lemons
+                self.lemonSupply += 1
+                
+                //update cash
+                self.cashSupply -= self.LEMON_PRICE
+            }else{
+                //Alert HERE OR SET ERROR MESSAGE
+                println("CAN NOT AFFORD ADDITIONAL LEMON")
+            }
+    }
+    
+    func unPurchaseLemon() {
+            if(self.lemonSupply > 1){
+                //Update Lemons
+                self.lemonSupply -= 1
+                
+                //update cash
+                self.cashSupply += self.LEMON_PRICE
+            }
+    }
+    
+    
+    func purchaseIceCube() {
+        if self.ICE_PRICE  <= cashSupply {
+            //Update Lemons
+            self.iceSupply += 1
+            
+            //Update cash
+            self.cashSupply -= self.ICE_PRICE
+        }else{
+            //Alert HERE OR SET ERROR MESSAGE
+            println("CAN NOT AFFORD ADDITIONAL ICE CUBE")
+        }
+    }
+    
+    func unPurchaseIceCube() {
+        if(self.iceSupply > 1){
+            //Update IceCubes
+            self.iceSupply -= 1
+            
+            //Update cash
+            self.cashSupply += self.ICE_PRICE
+        }
     }
  
-    
     //Mix
     @IBAction func lemonMixValueChanged(sender: UIStepper) {
         self.lemonsToMix = Int(sender.value)
@@ -88,6 +155,8 @@ class ViewController: UIViewController {
         //TODO:Update INVENTORY
         updateMainView()
     }
+    
+    
     
     //Sell Action
     @IBAction func startDayPressed(sender: UIButton) {
@@ -111,6 +180,17 @@ class ViewController: UIViewController {
         //Update Mix
         self.lemonMixLabel.text = "\(lemonsToMix)"
         self.iceMixLabel.text = "\(iceToMix)"
+    }
+    
+    func showAlertWithText(
+        header: String = "Warning",
+        message: String,
+        handler: ((UIAlertAction!) -> Void)? = nil,
+        completion: (() -> Void)? = nil){
+            
+            var alert  = UIAlertController(title: header, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: handler))
+            self.presentViewController(alert, animated: true, completion: completion)
     }
 
 
